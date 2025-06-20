efce13d82fb20b7adc25120f8b87bc03e071f5c3
Nikita Mitasov <mitanick@ya.ru>
2025-06-20 17:41:03 +0300

    home: use home-dotfiles-configuration and stow dir

--- home/home.scm
+++ home/home.scm
@@ -3,12 +3,13 @@
              (gnu home services)
              (gnu home services sound)
              (gnu home services desktop)
-	     (guix gexp)
-	     (guix packages)
-	     (guix download)
-	     (nongnu packages nvidia))
+             (gnu home services dotfiles)
+             (guix gexp)
+             (guix packages)
+             (guix download)
+             (nongnu packages nvidia))
 
-(define (home-dir) 
+(define (home-dir)
   (getenv "HOME"))
 
 (define (xdg-data-home)
@@ -21,46 +22,36 @@
     (dirname abs-path)))
 
 (define (dotfile path)
-  (local-file (string-append  config-root "/dotfiles/" path)))
+  (local-file (string-append config-root "/dotfiles/" path)))
 
 (home-environment
  (packages (load "packages.scm"))
 
  (services
-  (list
-   (service home-dbus-service-type)
+  (list (service home-dbus-service-type)
 
-   (simple-service
-    'env-vars-service
-    home-environment-variables-service-type
-    '(("TERM" . "xterm-256color")("NIXPKGS_ALLOW_UNFREE" . "1")))
+        (simple-service 'env-vars-service
+                        home-environment-variables-service-type
+                        '(("TERM" . "xterm-256color")
+                          ("NIXPKGS_ALLOW_UNFREE" . "1")))
 
-   (simple-service 'dotfiles
-		   home-files-service-type 
-		   `((".zshrc" ,(dotfile "zsh/zshrc"))
-		     (".zprofile" ,(dotfile "zsh/zprofile"))
-		     (".config/starship.toml", (dotfile "starship/starship.toml"))
-		     (".config/fastfetch/config.jsonc" ,(dotfile "fastfetch/config.jsonc"))
-		     (".config/sway/config" ,(dotfile "sway/config"))
-		     (".config/fastfetch/shika_guix.png" ,(dotfile "fastfetch/shika_guix.png"))
-		     (".config/nvim/init.lua" ,(dotfile "nvim/init.lua"))
-		     (".config/waybar/config.jsonc" ,(dotfile "waybar/config.jsonc"))
-		     (".config/waybar/style.css" ,(dotfile "waybar/style.css"))
-		     (".config/nixpkgs/config.nix" ,(dotfile "nix/nixpkgs-config.nix"))
-		     (".config/nix/nix.conf" ,(dotfile "nix/nix.conf"))
-		     (".config/rofi/config.rasi" ,(dotfile "rofi/config.rasi"))
-		     (".local/share/rofi/themes/custom.rasi" ,(dotfile "rofi/custom.rasi"))
-		     (".config/ghostty/config" ,(dotfile "ghostty/config"))
-		     (".config/ghostty/themes/ch4og" ,(dotfile "ghostty/themes/ch4og"))
-		     ))
+        (service home-dotfiles-service-type
+                 (home-dotfiles-configuration (directories '("./dotfiles"))
+                                              (layout 'stow)
+					      (excluded '(".*~" ".*\\.swp" "\\.git" "\\.gitignore" "README.md"))
+                                              (packages '("zsh" "starship"
+                                                          "fastfetch"
+                                                          "sway"
+                                                          "nvim"
+                                                          "waybar"
+                                                          "nix"
+                                                          "rofi"
+                                                          "ghostty"))))
 
-   (simple-service 'nix-channel-init
-  		   home-activation-service-type
-  		   #~(begin
-		       (use-modules (guix gexp))
-  		       (system* "nix-channel" "--add" "https://nixos.org/channels/nixpkgs-unstable" "nixpkgs")
-  		       (system* "nix-channel" "--update")
-		       )
-		   ))
-  
-  )
+        (simple-service 'nix-channel-init home-activation-service-type
+                        #~(begin
+                            (use-modules (guix gexp))
+                            (system* "nix-channel" "--add"
+				     "https://nixos.org/channels/nixpkgs-unstable"
+				     "nixpkgs")
+                            (system* "nix-channel" "--update"))))))
